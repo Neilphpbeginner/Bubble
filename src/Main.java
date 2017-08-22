7cad01c9e3bf78e2dadd4a741213c394974e93e0
Neil.Lemmer <Neil.Lemmer@GENBDFLT-110.babcock.co.za>
2017-08-22 14:49:07 +0200

    JTextFlieds and JLabels Setup Done. Method next

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,47 @@
 import java.awt.Dimension;
-import java.awt.FlowLayout;
-
 import javax.swing.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		System.out.println("Neil");
-		JFrame	frame	=	new JFrame();
-		JLabel redLabel	=	new JLabel("Red");
-		JLabel greenLabel	=	new JLabel("Green");
-		JLabel blueLabel	=	new JLabel("Blue");
-		
-		JTextField	red	=	new JTextField(10);
-		JTextField	green	=	new JTextField(10);
-		JTextField	blue	=	new JTextField(10);
-		
-		
-		redLabel.setLabelFor(red);
+		JFrame	frame		=	new JFrame();
+		Bubble 	b1 			=	new Bubble();
+		JLabel 	redLabel	=	new JLabel("Red");
+		JLabel 	greenLabel	=	new JLabel("Green");
+		JLabel 	blueLabel	=	new JLabel("Blue");
+		JButton	btn1		=	new JButton("Submit");
+		JTextField	red		=	new JTextField(5);
+		JTextField	green	=	new JTextField(5);
+		JTextField	blue	=	new JTextField(5);
 		JSlider	s1	=	new JSlider(0,200,100);
+		
 		s1.setPreferredSize(new Dimension(750,100));
 		s1.setVisible(true);
+	
 		
 			s1.setPaintTicks(true);
 			s1.setMajorTickSpacing(10);
 			s1.setMinorTickSpacing(10);
 			s1.setPaintLabels(true);
-			Bubble b1 	=	new Bubble();
-			b1.setLayout(new FlowLayout(FlowLayout.TRAILING));
-			s1.addChangeListener(b1);
-			b1.add(s1);
-			redLabel.setLabelFor(red);
-			greenLabel.setLabelFor(green);
-			blueLabel.setLabelFor(blue);
+			
+			
 			b1.add(redLabel);
+			b1.add(red);
+			b1.add(blueLabel);
+			b1.add(blue);
 			b1.add(greenLabel);
 			b1.add(green);
-			b1.add(red);
-			
+			b1.add(btn1);
+			b1.add(s1);
 			
 		
 			frame.setVisible(true);
 			frame.setSize(800, 600);
 		
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//			frame.add(s1);
+
 			
 			frame.add(b1);
 	}
