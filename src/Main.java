9d29b4818bbc90016e427e78ad1e39cfc8780895
Neil.Lemmer <Neil.Lemmer@GENBDFLT-110.babcock.co.za>
2017-08-22 13:55:15 +0200

    Setting up the JtextFields and JLabels I am going to use to modify colors with

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.awt.Dimension;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.*;
 
@@ -10,8 +9,16 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Neil");
 		JFrame	frame	=	new JFrame();
-		JTextField	red	=	new JTextField("Red");
-		red.setSize(3, 0);
+		JLabel redLabel	=	new JLabel("Red");
+		JLabel greenLabel	=	new JLabel("Green");
+		JLabel blueLabel	=	new JLabel("Blue");
+		
+		JTextField	red	=	new JTextField(10);
+		JTextField	green	=	new JTextField(10);
+		JTextField	blue	=	new JTextField(10);
+		
+		
+		redLabel.setLabelFor(red);
 		JSlider	s1	=	new JSlider(0,200,100);
 		s1.setPreferredSize(new Dimension(750,100));
 		s1.setVisible(true);
@@ -21,16 +28,25 @@ public class Main {
 			s1.setMinorTickSpacing(10);
 			s1.setPaintLabels(true);
 			Bubble b1 	=	new Bubble();
-			b1.setLayout(new GridLayout(2, 3));
+			b1.setLayout(new FlowLayout(FlowLayout.TRAILING));
 			s1.addChangeListener(b1);
 			b1.add(s1);
+			redLabel.setLabelFor(red);
+			greenLabel.setLabelFor(green);
+			blueLabel.setLabelFor(blue);
+			b1.add(redLabel);
+			b1.add(greenLabel);
+			b1.add(green);
 			b1.add(red);
+			
 			
 		
 			frame.setVisible(true);
 			frame.setSize(800, 600);
+		
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 //			frame.add(s1);
+			
 			frame.add(b1);
 	}
 
