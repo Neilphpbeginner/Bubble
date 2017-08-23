ee27ac8d6e96ede0359be4f85dce8f2a3edc5744
Neil.Lemmer <Neil.Lemmer@GENBDFLT-110.babcock.co.za>
2017-08-23 07:11:03 +0200

    Button Working changing colors on submit

--- src/Bubble.java
+++ src/Bubble.java
@@ -1,46 +1,37 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
-
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class Bubble extends JPanel implements ChangeListener{
-	
-		int raduis	=	0;
-		Random x	=	new Random(); 
-		int r,g,b;
+public class Bubble extends JPanel implements ChangeListener {
+
+	private static final long serialVersionUID = 1L;
+	private int raduis = 100;
+	private int r, g, b;
+
+	public void setColorParms(int red, int blue, int green) {
+		this.r = red;
+		this.b = blue;
+		this.g = green;
+	}
 
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
-		
-			JSlider source	= (JSlider)e.getSource();
-			
-				raduis	=	source.getValue();
-				repaint();
-		
+		JSlider source = (JSlider) e.getSource();
+		raduis = source.getValue();
+		repaint();
 	}
 
 	@Override
 	public void paint(Graphics arg0) {
-		// TODO Auto-generated method stub
 		super.paint(arg0);
-		
-		r	=	x.nextInt(255);
-		g	=	x.nextInt(255);
-		b	=	x.nextInt(255);
-		Color	myColor	=	new Color(r,g,b);
+		Color myColor = new Color(this.r, this.g, this.b);
 		arg0.setColor(myColor);
-		
-		int x	=	((super.getWidth()/2)-(raduis/2));
-		int y	=	((super.getHeight()/2)-(raduis/2));
-		if(raduis	== 0 ) {
-		arg0.fillOval(x, y, 50, 50);
-		} else {
-			arg0.fillOval(x, y, raduis, raduis);
-		}
-	}
+		repaint();
+		int x = ((super.getWidth() / 2) - (raduis / 2));
+		int y = ((super.getHeight() / 2) - (raduis / 2));
+		arg0.fillOval(x, y, raduis, raduis);
+	}	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,68 @@
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Main {
-
-	public static void main(String[] args) {
+public class Main extends JFrame implements ActionListener {
 	
-		System.out.println("Neil");
-		JFrame	frame		=	new JFrame();
-		Bubble 	b1 			=	new Bubble();
-		JLabel 	redLabel	=	new JLabel("Red");
-		JLabel 	greenLabel	=	new JLabel("Green");
-		JLabel 	blueLabel	=	new JLabel("Blue");
-		JButton	btn1		=	new JButton("Submit");
-		JTextField	red		=	new JTextField(5);
-		JTextField	green	=	new JTextField(5);
-		JTextField	blue	=	new JTextField(5);
-		JSlider	s1	=	new JSlider(0,200,100);
-		
-		s1.setPreferredSize(new Dimension(750,100));
-		s1.setVisible(true);
-	
-		
-			s1.setPaintTicks(true);
-			s1.setMajorTickSpacing(10);
-			s1.setMinorTickSpacing(10);
-			s1.setPaintLabels(true);
-			
-			
-			b1.add(redLabel);
-			b1.add(red);
-			b1.add(blueLabel);
-			b1.add(blue);
-			b1.add(greenLabel);
-			b1.add(green);
-			b1.add(btn1);
-			b1.add(s1);
-			
-		
-			frame.setVisible(true);
-			frame.setSize(800, 600);
-		
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	private static final long serialVersionUID = 1L;
+	JTextField redT 	= new JTextField(5);
+	JTextField greenT 	= new JTextField(5);
+	JTextField blueT 	= new JTextField(5);
+	Bubble b1 			= new Bubble();
+	private int red, blue, green;
 
-			
-			frame.add(b1);
+	public void setColors() {
+		red		= Integer.parseInt(String.valueOf(redT.getText()));
+		green 	= Integer.parseInt(String.valueOf(greenT.getText()));
+		blue 	= Integer.parseInt(String.valueOf(blueT.getText()));
+		b1.setColorParms(red, blue, green);
 	}
 
+	public Main() {
+		JButton btn1 		= new JButton("Submit");
+		JLabel  redLabel 	= new JLabel("Red");
+		JLabel  greenLabel 	= new JLabel("Green");
+		JLabel  blueLabel 	= new JLabel("Blue");
+		JSlider s1 			= new JSlider(0, 200, 100);
+		
+		btn1.addActionListener(this);
+
+		s1.setPreferredSize(new Dimension(750, 100));
+		s1.setVisible(true);
+		s1.setPaintTicks(true);
+		s1.setMajorTickSpacing(10);
+		s1.setMinorTickSpacing(10);
+		s1.setPaintLabels(true);
+		s1.addChangeListener(b1);
+		
+		b1.add(redLabel);
+		b1.add(redT);
+		b1.add(blueLabel);
+		b1.add(blueT);
+		b1.add(greenLabel);
+		b1.add(greenT);
+		b1.add(btn1);
+		b1.add(s1);
+		add(b1);
+	}
+
+	public static void createGUI() {
+		JFrame frame = new Main();
+		frame.setVisible(true);
+		frame.setSize(800, 600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		if (command.equals("Submit")) {
+			setColors();
+		}
+	}
+
+	public static void main(String[] args) {
+		createGUI();
+	}
 }
